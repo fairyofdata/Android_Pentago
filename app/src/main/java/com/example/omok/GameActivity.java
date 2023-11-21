080095e686bab6030842a01c105665de0e66e7e8
ChoHangMin <slightsmille@gmail.com>
2023-11-21 15:14:40 +0900

    Update GameActivity add dynamic button -v1.2.0

--- app/src/main/java/com/example/omok/GameActivity.java
+++ app/src/main/java/com/example/omok/GameActivity.java
@@ -6,7 +6,10 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.GridView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
+
+import android.content.Intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,6 +19,7 @@ public class GameActivity extends AppCompatActivity {
 
     private pentagoAdapter adapter;
     private int currentPlayer = 0; // 0은 흑, 1은 백
+    private int matchFlag = 0;
     private int selectedRow, selectedCol;
 
     private int areaRow, areaCol;
@@ -23,7 +27,10 @@ public class GameActivity extends AppCompatActivity {
 
     private int[][] quarterBoard = new int[3][3];
     private int[][] rotateState = new int[3][3];
-    private Button placeCompleteButton,onRotationSensorButton,offRotationSensorButton;
+    private Button placeCompleteButton;
+    private LinearLayout ActivityLayout;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,10 +38,8 @@ public class GameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game);
 
         adapter = new pentagoAdapter(this);
-
+        ActivityLayout = findViewById(R.id.linearLayout);
         placeCompleteButton = findViewById(R.id.placeCompleteButton);
-        onRotationSensorButton = findViewById(R.id.onRotationSensorButton);
-        offRotationSensorButton = findViewById(R.id.offRotationSensorButton);
 
         for (int i = 0; i < 6; i++) {
             for (int j = 0; j < 6; j++) {
@@ -46,40 +51,14 @@ public class GameActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                match();
 
+                if (boardState[selectedRow][selectedCol] == 10) {
+                    matchPlaceStone();
+                } else {
+                    Toast.makeText(GameActivity.this, "Select position Again.", Toast.LENGTH_SHORT).show();
+                }
             }
         });
-
-        onRotationSensorButton.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-
-                rotateState90Right();
-                adapter.placeRotationArea(areaRow, areaCol, rotateState);
-                showQuarterState();
-                showRotateState();
-
-
-
-            }
-        });
-
-        offRotationSensorButton.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-
-                rotateState90Left();
-                adapter.placeRotationArea(areaRow, areaCol, rotateState);
-                showQuarterState();
-                showRotateState();
-
-            }
-        });
-
-
 
 
         gridView = findViewById(R.id.gridView);
@@ -89,6 +68,7 @@ public class GameActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
                 setClickPosition(position);
+                setRotationArea(selectedRow, selectedCol);
 
             }
         });
@@ -104,21 +84,104 @@ public class GameActivity extends AppCompatActivity {
 
     }
 
+    private void turn() {
+        adapter.placeStone(selectedRow, selectedCol, currentPlayer);
+        updateBoarState(selectedRow, selectedCol, currentPlayer);
+        addButton();
+        Log.d("Debug", "turn() - Button added");
+    }
+
+    private void addButton() {
+
+        Button onRotationSensorButton = new Button(this);
+        Button offRotationSensorButton = new Button(this);
+
+        onRotationSensorButton.setText("On Rotation Sensor");
+        offRotationSensorButton.setText("Off Rotation Sensor");
+
+
+        onRotationSensorButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+
+
+                rotateState90Right();
+                adapter.placeRotationArea(areaRow, areaCol, rotateState);
+                showQuarterState();
+                showRotateState();
+                changedBoardState();
+                //checkWinner();
+                removeButton(onRotationSensorButton, offRotationSensorButton);
+
+
+            }
+        });
+
+
+
+        offRotationSensorButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+
+
+                rotateState90Left();
+                adapter.placeRotationArea(areaRow, areaCol, rotateState);
+                showQuarterState();
+                showRotateState();
+                changedBoardState();
+                checkWinner();
+                removeButton(onRotationSensorButton, offRotationSensorButton);
+
+
+
+
+
+            }
+        });
+
+
+
+        ActivityLayout.addView(onRotationSensorButton);
+        ActivityLayout.addView(offRotationSensorButton);
+        Toast.makeText(this, "Button Added", Toast.LENGTH_SHORT).show();
+        Log.d("Debug", "addButton() - Buttons added to layout");
+
+    }
+
+    private void removeButton(Button onRotationSensorButton, Button offRotationSensorButton) {
+        ActivityLayout.removeView(onRotationSensorButton);
+        ActivityLayout.removeView(offRotationSensorButton);
+        Log.d("Debug", "removeButton() - Buttons removed from layout");
+    }
+
+    private void addOffRotationButton() {
+
+
+    }
+
     private void togglePlayer() {
         currentPlayer = (currentPlayer == 0) ? 1 : 0;
     }
 
-    private void match() {
+    private void toggleFlag() {
+        matchFlag = (matchFlag == 0) ? 1 : 0;
+    }
+
+    private void matchPlaceStone() {
         turn();
-        setRotationArea(selectedRow, selectedCol);
         togglePlayer();
 
     }
 
-    private void turn() {
-        adapter.placeStone(selectedRow, selectedCol, currentPlayer);
-        updateBoarState(selectedRow, selectedCol, currentPlayer);
+    private void matchRotateStone() {
+
     }
+
+
 
     private void updateBoarState(int selectedRow, int selectedCol, int currentPlayer) {
         boardState[selectedRow][selectedCol] = (currentPlayer == 0) ? 0 : 1;
@@ -149,6 +212,7 @@ public class GameActivity extends AppCompatActivity {
                 quarterBoard[i][j] = boardState[areaRow * 3 + i][areaCol * 3 + j];
             }
         }
+        showQuarterState();
 
     }
 
@@ -171,9 +235,163 @@ public class GameActivity extends AppCompatActivity {
 
     }
     
+
     private void checkWinner() {
 
+        int winnerPlayer = 10;
+
+        winnerPlayer = checkHorizontalWinner();
+        alertWinner(winnerPlayer);
+        winnerPlayer = checkVertical();
+        alertWinner(winnerPlayer);
+        winnerPlayer = checkLeftDiagonal();
+        alertWinner(winnerPlayer);
+        winnerPlayer = checkRightDiagonal();
+        alertWinner(winnerPlayer);
+
     }
+
+
+
+    private void alertWinner(int winnerPlayer) {
+
+        String winnerColor;
+        if (winnerPlayer == 0) {
+            winnerColor = "Black";
+        } else if (winnerPlayer == 1) {
+            winnerColor = "White";
+        } else {
+            return;
+        }
+
+        Toast.makeText(this, winnerColor + "is Winner!", Toast.LENGTH_SHORT).show();
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private int checkHorizontalWinner() {
+
+        int checkSum = 0;
+
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 2; j++) {
+                for (int k = 0; k < 5; k++) {
+                    checkSum += boardState[i][j + k];
+                    if (checkSum == 0) {
+                        return 0;
+                    } else if (checkSum == 5) {
+                        return 1;
+                    }
+                }
+            }
+        }
+        return 10;
+
+    }
+
+    private int checkVertical() {
+        int checkSum = 0;
+
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 2; j++) {
+                for (int k = 0; k < 5; k++) {
+                    checkSum += boardState[j + k][i];
+                    if (checkSum == 0) {
+                        return 0;
+                    } else if (checkSum == 5) {
+                        return 1;
+                    }
+                }
+            }
+        }
+        return 10;
+
+    }
+
+    private int checkRightDiagonal() {
+
+        int checkSum = 0;
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                for (int k = 0; k < 5; k++) {
+                    checkSum += boardState[i + k][j + k];
+                    if (checkSum == 0) {
+                        return 0;
+                    } else if (checkSum == 5) {
+                        return 1;
+                    }
+
+                }
+            }
+        }
+
+        return 10;
+
+    }
+
+    private int checkLeftDiagonal() {
+
+        int checkSum = 0;
+
+        for (int i = 4; i < 6; i++) {
+            for (int j = 4; j < 6; j++) {
+                for (int k = 0; k < 5; k++) {
+                    checkSum += boardState[i + k][j + k];
+                    if (checkSum == 0) {
+                        return 0;
+                    } else if (checkSum == 5) {
+                        return 1;
+                    }
+
+                }
+            }
+        }
+
+        return 10;
+
+    }
+
+    private void changedBoardState() {
+        showBoardState();
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                boardState[3 * areaRow + i][3 * areaCol + j] = rotateState[i][j];
+            }
+        }
+        showChangedBoardState();
+    }
+
+    private void showBoardState() {
+        Log.d("BoardState", "=====================");
+        for (int i = 0; i < 6; i++) {
+            StringBuilder row = new StringBuilder("| ");
+            for (int j = 0; j < 6; j++) {
+                row.append(boardState[i][j]).append(" | ");
+            }
+            Log.d("BoardState", row.toString());
+        }
+        Log.d("BoardState", "=====================");
+
+
+    }
+
+    private void showChangedBoardState() {
+        Log.d("ChangedBoardState", "=====================");
+        for (int i = 0; i < 6; i++) {
+            StringBuilder row = new StringBuilder("| ");
+            for (int j = 0; j < 6; j++) {
+                row.append(boardState[i][j]).append(" | ");
+            }
+            Log.d("ChangedBoardState", row.toString());
+        }
+        Log.d("ChangedBoardState", "=====================");
+
+    }
+
+
 
     private void showQuarterState() {
         Log.d("QuarterState", "=====================");
